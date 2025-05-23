194553790af5fb100b43ee65e542f01b164c8deb
Mariusz Szczepanik <mua@spyro-soft.com>
2025-05-23 15:21:32 +0200

    Fix actions
    Signed-off-by: Mariusz Szczepanik <mua@spyro-soft.com>

--- rtest/include/rtest/action_client_mock.hpp
+++ rtest/include/rtest/action_client_mock.hpp
@@ -1,20 +1,16 @@
 #pragma once
-
 #include <gmock/gmock.h>
 #include <rtest/static_registry.hpp>
 #include <rtest/action_client_base.hpp>
 #include "rclcpp/node_interfaces/node_base_interface.hpp"
+#include "rclcpp/node_interfaces/node_graph_interface.hpp"
 #include <future>
 
 #define TEST_TOOLS_MAKE_SHARED_DEFINITION(...) \
   template<typename ... Args> \
-  static std::shared_ptr<__VA_ARGS__> \
-  make_shared(Args && ... args) \
-  { \
+  static std::shared_ptr<__VA_ARGS__> make_shared(Args && ... args) { \
     auto ptr = std::make_shared<__VA_ARGS__>(std::forward<Args>(args) ...); \
-    ptr->post_init_setup(); \
-    return ptr; \
-  }
+    ptr->post_init_setup(); return ptr; }
 
 #define TEST_TOOLS_SMART_PTR_DEFINITIONS(...) \
   __RCLCPP_SHARED_PTR_ALIAS(__VA_ARGS__) \
@@ -23,7 +19,6 @@
   TEST_TOOLS_MAKE_SHARED_DEFINITION(__VA_ARGS__)
 
 namespace rclcpp_action {
-
 template<typename ActionT>
 class ClientGoalHandle {
 public:
@@ -45,8 +40,7 @@ public:
 };
 
 template<typename ActionT>
-class Client : public ClientBase,
-              public std::enable_shared_from_this<Client<ActionT>> {
+class Client : public ClientBase, public std::enable_shared_from_this<Client<ActionT>> {
 public:
   using Goal = typename ActionT::Goal;
   using Feedback = typename ActionT::Feedback;
@@ -54,17 +48,12 @@ public:
   using GoalHandle = ClientGoalHandle<ActionT>;
   using GoalHandleSharedPtr = typename GoalHandle::SharedPtr;
   using WrappedResult = typename GoalHandle::WrappedResult;
-
   using GoalResponseCallback = std::function<void(GoalHandleSharedPtr)>;
   using FeedbackCallback = typename GoalHandle::FeedbackCallback;
   using ResultCallback = typename GoalHandle::ResultCallback;
 
   struct SendGoalOptions {
-    SendGoalOptions()
-    : goal_response_callback(nullptr),
-      feedback_callback(nullptr),
-      result_callback(nullptr) {}
-
+    SendGoalOptions() : goal_response_callback(nullptr), feedback_callback(nullptr), result_callback(nullptr) {}
     GoalResponseCallback goal_response_callback;
     FeedbackCallback feedback_callback;
     ResultCallback result_callback;
@@ -73,24 +62,18 @@ public:
   Client(
     rclcpp::node_interfaces::NodeBaseInterface::SharedPtr node_base,
     rclcpp::node_interfaces::NodeGraphInterface::SharedPtr node_graph,
-    rclcpp::node_interfaces::NodeLoggingInterface::SharedPtr node_logging,
     const std::string & action_name)
-  : node_base_(node_base), action_name_(action_name)
-  {
+  : node_base_(node_base), action_name_(action_name) {
     (void)node_graph;
-    (void)node_logging;
   }
 
   void post_init_setup() {
     rtest::StaticMocksRegistry::instance().template registerActionClient<ActionT>(
-      node_base_->get_fully_qualified_name(),
-      action_name_,
-      this->shared_from_this());
+      node_base_->get_fully_qualified_name(), action_name_, this->shared_from_this());
   }
 
   std::shared_future<GoalHandleSharedPtr> async_send_goal(const Goal& goal, const SendGoalOptions& options) {
-    (void)goal;
-    (void)options;
+    (void)goal; (void)options;
     std::promise<GoalHandleSharedPtr> promise;
     promise.set_value(nullptr);
     return promise.get_future().share();
@@ -110,11 +93,9 @@ private:
   rclcpp::node_interfaces::NodeBaseInterface::SharedPtr node_base_;
   std::string action_name_;
 };
-
 } // namespace rclcpp_action
 
 namespace rtest {
-
 template<typename ActionT>
 class ActionClientMock : public MockBase {
 public:
@@ -126,34 +107,20 @@ public:
 
   explicit ActionClientMock(rclcpp_action::ClientBase* client) : client_(client) {}
   ~ActionClientMock() { StaticMocksRegistry::instance().detachMock(client_); }
-
   TEST_TOOLS_SMART_PTR_DEFINITIONS(ActionClientMock<ActionT>)
 
-  // Core mocks
-  MOCK_METHOD(std::shared_future<GoalHandleSharedPtr>,
-              async_send_goal,
-              (const Goal&, const SendGoalOptions&), ());
+  MOCK_METHOD(std::shared_future<GoalHandleSharedPtr>, async_send_goal, (const Goal&, const SendGoalOptions&), ());
+  MOCK_METHOD(std::shared_future<WrappedResult>, async_get_result, (const GoalHandleSharedPtr&), ());
+  MOCK_METHOD(bool, action_server_is_ready, (), ());
 
-  MOCK_METHOD(std::shared_future<WrappedResult>,
-              async_get_result,
-              (const GoalHandleSharedPtr&), ());
-
-  MOCK_METHOD(bool,
-              action_server_is_ready,
-              (), ());
-
-  void simulate_feedback(
-      const GoalHandleSharedPtr& goal_handle,
-      const typename ActionT::Feedback& feedback) {
+  void simulate_feedback(const GoalHandleSharedPtr& goal_handle, const typename ActionT::Feedback& feedback) {
     if (goal_handle && goal_handle->feedback_callback) {
       auto feedback_msg = std::make_shared<const typename ActionT::Feedback>(feedback);
       goal_handle->feedback_callback(goal_handle, feedback_msg);
     }
   }
 
-  void simulate_result(
-      const GoalHandleSharedPtr& goal_handle,
-      const WrappedResult& result) {
+  void simulate_result(const GoalHandleSharedPtr& goal_handle, const WrappedResult& result) {
     if (goal_handle && goal_handle->result_callback) {
       goal_handle->result_callback(result);
     }
@@ -164,14 +131,9 @@ private:
 };
 
 template<typename ActionT>
-std::shared_ptr<ActionClientMock<ActionT>> findActionClient(
-    const std::string& fullyQualifiedNodeName,
-    const std::string& actionName) {
+std::shared_ptr<ActionClientMock<ActionT>> findActionClient(const std::string& fullyQualifiedNodeName, const std::string& actionName) {
   std::shared_ptr<ActionClientMock<ActionT>> client_mock{};
-  auto client_base = StaticMocksRegistry::instance()
-                        .getActionClient(fullyQualifiedNodeName, actionName)
-                        .lock();
-
+  auto client_base = StaticMocksRegistry::instance().getActionClient(fullyQualifiedNodeName, actionName).lock();
   if (client_base) {
     if (StaticMocksRegistry::instance().getMock(client_base.get()).lock()) {
       std::cerr << "WARNING: ActionClientMock already attached\n";
@@ -184,10 +146,7 @@ std::shared_ptr<ActionClientMock<ActionT>> findActionClient(
 }
 
 template<typename ActionT, typename NodeT>
-std::shared_ptr<ActionClientMock<ActionT>> findActionClient(
-    const std::shared_ptr<NodeT>& node,
-    const std::string& actionName) {
+std::shared_ptr<ActionClientMock<ActionT>> findActionClient(const std::shared_ptr<NodeT>& node, const std::string& actionName) {
   return findActionClient<ActionT>(node->get_fully_qualified_name(), actionName);
 }
-
 } // namespace rtest
